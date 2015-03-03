924778334c53c0da2a607b98d2df12ac8033dc48
Eric Chee <bboyairwreck@gmail.com>
2015-03-03 02:53:33 -0800

    Unlimited cards with onTouch unregistration and removes card views after swipe

--- app/src/main/java/edu/washington/echee/swipeviews/MainActivity.java
+++ app/src/main/java/edu/washington/echee/swipeviews/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.RotateAnimation;
 import android.view.animation.TranslateAnimation;
@@ -15,13 +16,15 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity implements View.OnTouchListener {
     ViewGroup _root;                // root view
-    LinearLayout cardView1;         // current swipable card
     private int _xDelta;            // distance card dragged in X-axis
     private int screenWidth = 0;    // width of rootView
     private float pivotX;           // rotation pivot X position
     private float pivotY;           // rotation pivot Y position
+    private int numCards;
 
     public static final float MAX_ROTATION = 17f;   // max degrees to rotate card
+    public static final int MAX_CARDS = 10;         // max number of cards on screen at a time
+    public static final int MIN_CARDS = 3;         // min number of cards on screen at a time
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +33,7 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 
         // Get Root container
         _root = (ViewGroup) findViewById(R.id.root);
-
-        // Create card view
-        cardView1 = new LinearLayout(this);
-        cardView1.setBackgroundColor(Color.GREEN);
-
-        // give it dummy height & width (change it later in onWindowFocusChanged())
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(300, 300); // width & height
-        cardView1.setLayoutParams(layoutParams);
-        _root.addView(cardView1);
-
-        _root.setOnTouchListener(this);
+        this.numCards = MAX_CARDS;
     }
 
     /*
@@ -50,25 +43,51 @@ public class MainActivity extends Activity implements View.OnTouchListener {
     @Override
     public void onWindowFocusChanged(boolean b) {
         this.screenWidth = _root.getWidth();
-        int cardWidth = this.screenWidth-32;
-        int height = 300;
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(cardWidth, height); // width & height
-        layoutParams.leftMargin = 16;
-        layoutParams.topMargin = 16;
-        cardView1.setLayoutParams(layoutParams);
-
-        this.pivotX = cardWidth / 2;    // set x pivot point center
-        this.pivotY = height * 2f;      // set y pivot point 2x below the height
-
-        // set pivot location to bottom center
-        cardView1.setPivotX(pivotX);
-        cardView1.setPivotY(pivotY);
-
+        addCards(MAX_CARDS);
     }
 
+    /*
+     * Adds @size number of cards to root view. Each card will be given an onClickListener
+     * Requirement:
+     *      Must be done AFTER root view is loaded i.e. onWindowFocus() but NOT in onCreate()
+     */
+    public void addCards(int size) {
+        int cardWidth = this.screenWidth-32;    // width of each card
+        int height = cardWidth;                       // height of each card
 
+        // Create cards in add to root view
+        for (int i = 0; i < size; i++) {
+            LinearLayout card = new LinearLayout(this);
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(cardWidth, height); // width & height
+            layoutParams.leftMargin = 16;
+            layoutParams.topMargin = 16;
+            card.setLayoutParams(layoutParams);
+
+            // TODO Delete. Each card alternates between Green & RED
+            card.setBackgroundColor(Color.RED);
+            if (i%2==0) {
+                card.setBackgroundColor(Color.GREEN);
+            }
+            // TODO DELETE ^
+
+            this.pivotX = cardWidth / 2;    // set x pivot point center
+            this.pivotY = height * 2f;      // set y pivot point 2x below the height
+
+            // set pivot location to bottom center
+            card.setPivotX(pivotX);
+            card.setPivotY(pivotY);
+
+            // Add card to root view & set onTouchListener
+            _root.addView(card, 0, layoutParams);
+            card.setOnTouchListener(this);
+        }
+    }
+
+    /*
+     * If detecting card swipe, this method will determine if current card should be removed or reset
+     */
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
+    public boolean onTouch(final View v, MotionEvent event) {
         final int X = (int) event.getRawX();    // X position of touch event
         int dif;                                // temp xDelta
         float swipePercent;                     // % of distance from center X to edge of screen
@@ -76,7 +95,7 @@ public class MainActivity extends Activity implements View.OnTouchListener {
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             // On Press Down
             case MotionEvent.ACTION_DOWN:
-                _xDelta = X - (int)this.cardView1.getTranslationX();    // track how far moved x
+                _xDelta = X - (int)v.getTranslationX();    // track how far moved x
                 break;
             // On Drag
             case MotionEvent.ACTION_MOVE:
@@ -84,14 +103,14 @@ public class MainActivity extends Activity implements View.OnTouchListener {
                 swipePercent = (float)dif / (float)screenWidth;
 
                 float rotation = swipePercent * MAX_ROTATION;
-                float alpha = (1f - Math.abs(swipePercent))*0.7f;
+                float alpha = (1f - Math.abs(swipePercent));
 
                 // set rotation, alpha, & translation
-                this.cardView1.setAlpha(alpha);
-                this.cardView1.setRotation(rotation);
-                this.cardView1.setTranslationX(dif);
+                v.setAlpha(alpha);
+                v.setRotation(rotation);
+                v.setTranslationX(dif);
 
-                Log.i("OnTouch", "swipePercent = " + swipePercent + "; rotation = " + rotation+ "; alpha = " + alpha);
+                Log.i("OnTouch", "swipePercent = " + swipePercent + "; rotation = " + rotation + "; alpha = " + alpha);
                 break;
             // On Release
             case MotionEvent.ACTION_UP:
@@ -99,16 +118,16 @@ public class MainActivity extends Activity implements View.OnTouchListener {
                 swipePercent = Math.abs((float) dif / (float) screenWidth);
 
                 // Reset card position if did NOT reach swipe threshold
-                if (swipePercent < 0.35f) {
-                    this.cardView1.setAlpha(1);
-                    this.cardView1.setRotation(0f);
-                    this.cardView1.setTranslationX(0);
+                if (swipePercent < 0.3f) {
+                    v.setAlpha(1);
+                    v.setRotation(0f);
+                    v.setTranslationX(0);
 
                 // Animate card off screen if did reach swipe threshold
                 } else {
                     int animDuration = 600;
                     float sign = 1;
-                    if (this.cardView1.getRotation() < 0) {
+                    if (v.getRotation() < 0) {
                         sign = -1;
                     }
                     RotateAnimation rotateAnim = new RotateAnimation(0, MAX_ROTATION*sign, pivotX, pivotY);
@@ -123,14 +142,67 @@ public class MainActivity extends Activity implements View.OnTouchListener {
                     animSet.addAnimation(rotateAnim);
                     animSet.addAnimation(translateAnim);
                     animSet.setFillAfter(true);
+                    animSet.setAnimationListener(new MyAnimationListener(v));
 
-                    this.cardView1.startAnimation(animSet);
+                    // Animate card off screen and remove it
+                    v.startAnimation(animSet);
                 }
 
                 Log.i("OnTouch", "Touch Released");
+
                 break;
         }
         _root.invalidate();
         return true;
+    }
+
+    /*
+     * If end of animation, removes card view from the root view.
+     * If number of cards in root < MIN_CARDS, then it will add more cards to the root view
+     */
+    public class MyAnimationListener implements Animation.AnimationListener {
+        View v;
+
+        public MyAnimationListener(View v) {
+            this.v = v;
+        }
+
+        /*
+         * Fired when Animation finishes
+         */
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            Log.i("onAnimationEnd", "Animation ended. Removing card");
+            v.setOnTouchListener(null);     // unregister onTouchListener
+
+            // Can only remove a view in Runnable UI Thread in AnimationListener
+            _root.post(new Runnable() {
+                public void run() {
+                    // it works without the runOnUiThread, but all UI updates must be done on the UI thread
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        public void run() {
+                            _root.removeView(v);        // removes card from root view
+                            Log.i("onAnimationEnd", "Card removed");
+
+                            MainActivity.this.numCards--; // decrement number of cards
+
+                            // If down to MIN_CARDS, add more cards
+                            if (numCards <= MIN_CARDS) {
+                                int numOfAddedCards = MAX_CARDS - numCards;
+                                MainActivity.this.addCards(numOfAddedCards);
+                                MainActivity.this.numCards += numOfAddedCards;
+
+                                Log.i("onAnimationEnd", "Added " + numOfAddedCards + " cards to root view");
+                            }
+                        }
+                    });
+                }
+            });
+        }
+
+        @Override
+        public void onAnimationStart(Animation animation) {}
+        @Override
+        public void onAnimationRepeat(Animation animation) {}
     }
 }
