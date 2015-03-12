9a496d75f24d7aed437278ccdcdfdc29e5579d20
Michael Slagle <mislagle@outlook.com>
2015-03-12 01:30:23 -0700

    Changed RestaurantDetails to include price, rating bar, and description, added in HungrBaseActivity extension in all Activities

--- app/src/main/java/edu/washington/echee/swipeviews/RestaurantDetails.java
+++ app/src/main/java/edu/washington/echee/swipeviews/RestaurantDetails.java
@@ -1,5 +1,6 @@
 package edu.washington.echee.swipeviews;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
@@ -8,18 +9,27 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.InputStream;
 
 
-public class RestaurantDetails extends ActionBarActivity {
+public class RestaurantDetails extends HungrBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_details);
+        super.createNavDrawer(this);
 
         ImageView imageView = (ImageView)findViewById(R.id.restaurantImage);
+        TextView title = (TextView)findViewById(R.id.restaurant_title);
+        TextView price = (TextView)findViewById(R.id.priceRange);
+        RatingBar ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        TextView descr = (TextView)findViewById(R.id.description);
 
         try{
             InputStream inputStream = getAssets().open("stock_restaurant.jpg");
@@ -29,6 +39,19 @@ public class RestaurantDetails extends ActionBarActivity {
             Log.e("RestaurantDetails","Couldn't load restaurant image -- probably IO exception.");
         }
 
+        HungrApp hungrApp = new HungrApp().getInstance();
+        Intent intent = getIntent();
+        Restaurant restaurant = hungrApp.getRestaurants().get(intent.getStringExtra("restaurant"));
+
+        title.setText(restaurant.title);
+        String priceBuilder = "";
+        for(int i = 0; i < restaurant.priceLevel; i++){
+            priceBuilder += "$";
+        }
+        price.setText(priceBuilder);
+        ratingBar.setNumStars(5);
+        ratingBar.setRating((float)restaurant.rating);
+        descr.setText(restaurant.desc);
     }
 
 
@@ -44,10 +67,7 @@ public class RestaurantDetails extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (super.onNavDrawerButtonPress(item)) { //Required to activate drawer
             return true;
         }
 
--- app/src/main/java/edu/washington/echee/swipeviews/RestaurantList.java
+++ app/src/main/java/edu/washington/echee/swipeviews/RestaurantList.java
@@ -18,12 +18,13 @@ import java.util.List;
 import java.util.Set;
 
 
-public class RestaurantList extends ActionBarActivity {
+public class RestaurantList extends HungrBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_list);
+        super.createNavDrawer(this);
 
         ListView restaurantListView = (ListView) findViewById(R.id.restaurantList);
         HungrApp hungrApp = (HungrApp)getApplicationContext();
@@ -65,10 +66,7 @@ public class RestaurantList extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (super.onNavDrawerButtonPress(item)) { //Required to activate drawer
             return true;
         }
 
