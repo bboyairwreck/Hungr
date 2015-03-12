49fbb83c19f4fc56b0293dd09378ee67ff4dd88a
Eric Chee <bboyairwreck@gmail.com>
2015-03-12 11:49:46 -0700

    Restaurant Imgs in details view load properly

--- app/src/main/java/edu/washington/echee/swipeviews/RestaurantDetails.java
+++ app/src/main/java/edu/washington/echee/swipeviews/RestaurantDetails.java
@@ -1,6 +1,9 @@
 package edu.washington.echee.swipeviews;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
@@ -18,6 +21,7 @@ import java.io.InputStream;
 
 
 public class RestaurantDetails extends HungrBaseActivity {
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +29,26 @@ public class RestaurantDetails extends HungrBaseActivity {
         setContentView(R.layout.activity_restaurant_details);
         super.createNavDrawer(this);
 
-        ImageView imageView = (ImageView)findViewById(R.id.restaurantImage);
+        HungrApp hungrApp = new HungrApp().getInstance();
+        Intent intent = getIntent();
+        Restaurant restaurant = hungrApp.getRestaurants().get(intent.getStringExtra("restaurant"));
+
+        imageView = (ImageView)findViewById(R.id.restaurantImage);
         TextView title = (TextView)findViewById(R.id.restaurant_title);
         TextView price = (TextView)findViewById(R.id.priceRange);
         RatingBar ratingBar = (RatingBar)findViewById(R.id.ratingBar);
         TextView descr = (TextView)findViewById(R.id.description);
 
-        try{
-            InputStream inputStream = getAssets().open("stock_restaurant.jpg");
-            Drawable drawableImage = Drawable.createFromStream(inputStream, null);
-            imageView.setImageDrawable(drawableImage);
-        } catch(Exception e){
-            Log.e("RestaurantDetails","Couldn't load restaurant image -- probably IO exception.");
+        String restImgDrawableName = restaurant.imgName;
+        int restaurantImageID = getResources().getIdentifier(restImgDrawableName , "drawable", getPackageName());
+
+        if (restaurantImageID <= 0) {
+            restaurantImageID = getResources().getIdentifier("stock_restaurant" , "drawable", getPackageName());
         }
 
-        HungrApp hungrApp = new HungrApp().getInstance();
-        Intent intent = getIntent();
-        Restaurant restaurant = hungrApp.getRestaurants().get(intent.getStringExtra("restaurant"));
+        imageView.setImageBitmap(
+                decodeSampledBitmapFromResource(getResources(), restaurantImageID, 100, 100));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
         title.setText(restaurant.title);
         String priceBuilder = "";
@@ -72,5 +79,51 @@ public class RestaurantDetails extends HungrBaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        System.gc();
+        imageView.setImageBitmap(null);
     }
 }
