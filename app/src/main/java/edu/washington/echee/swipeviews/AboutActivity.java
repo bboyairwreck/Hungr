2995c9fd35b9d5eb51ef1e2c8f5177d14cb2fa08
Steven Raden <stevengraden@gmail.com>
2015-03-12 01:52:12 -0700

    About page

--- app/src/main/java/edu/washington/echee/swipeviews/AboutActivity.java
+++ app/src/main/java/edu/washington/echee/swipeviews/AboutActivity.java
@@ -1,9 +1,13 @@
 package edu.washington.echee.swipeviews;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class AboutActivity extends HungrBaseActivity {
@@ -14,6 +18,10 @@ public class AboutActivity extends HungrBaseActivity {
         setContentView(R.layout.activity_about);
 
         super.createNavDrawer(this);
+        ImageView imageView = (ImageView) findViewById(R.id.about_image);
+        imageView.setImageBitmap(
+                decodeSampledBitmapFromResource(getResources(), R.drawable.programming, 200, 200));
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
 
     }
 
@@ -29,5 +37,44 @@ public class AboutActivity extends HungrBaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
     }
 }
